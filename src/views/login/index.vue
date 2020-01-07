75185a7532f4e2be5b1ec4d66f3a02e21ca95ba7
lb <cmds12345678@163.com>
2020-01-07 22:39:23 +0800

    备案信息

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -24,6 +24,12 @@
               <el-button @click="resetForm('ruleForm2')">重置</el-button>
             </el-form-item>
           </el-form>
+          <div>
+            <img src="../../assets/img/beian.png" alt="公安备案">     
+            <div style="width:300px;margin:0 auto; padding:20px 0;">
+              <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=51010702001623" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"><img src="" style="float:left;"/><p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">川公网安备 51010702001623号</p></a>
+            </div>            
+          </div>
       </el-col>
     </el-row>
     </div>
